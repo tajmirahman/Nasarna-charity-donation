edcf52f2db389b399e8e81a743d0a3730ba9af4c
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-10 14:08:30 +0600

    project is start now

---
+++ my-project/src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import {createBrowserRouter} from "react-router";
+
+// const { createBrowserRouter } = require("react-router-dom");
+
+
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<h2>This is main layout</h2>
+    }
+]);
+
+export default router;
