4107b049c6df9ec18be945150cc7d07ccb18f45e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-11 10:19:56 +0600

    our causes card design is running

--- my-project/src/Pages/OurCauses/CausesCard.jsx
+++ my-project/src/Pages/OurCauses/CausesCard.jsx
@@ -4,7 +4,10 @@ const CausesCard = ({causes}) => {
     console.log(causes)
     return (
         <div>
-            
+            <div>
+                <img src={causes.image} alt="" />
+                <h2>{causes.title}</h2>
+            </div>
         </div>
     );
 };
