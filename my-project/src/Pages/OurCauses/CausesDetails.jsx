f9b824e49ac7f701459e73f7cae263718f7e71d2
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-14 23:40:33 +0600

    card details problem is solve

--- my-project/src/Pages/OurCauses/CausesDetails.jsx
+++ my-project/src/Pages/OurCauses/CausesDetails.jsx
@@ -1,87 +1,92 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLoaderData, useLocation, useParams } from 'react-router-dom';
+import { Link, useLoaderData, useParams } from 'react-router-dom';
 import Header from '../../Components/Header';
 import CausesDetailsCard from './CausesDetailsCard';
 
 const CausesDetails = () => {
     const data = useLoaderData();
-    const {id}=useParams();
+    const { id } = useParams();
     // console.log(data,id);
-    const [details,setDetails]=useState([]);
+    const [details, setDetails] = useState([]);
 
-    useEffect(()=>{
-        const dataFind=data.find(d=> d.id == id);
+    useEffect(() => {
+        const dataFind = data.filter(d => d.id == id);
         setDetails(dataFind)
-    },[data, id])
+    }, [data, id])
 
 
 
     return (
         <>
 
-        <div>
+            <div >
 
-            <Header></Header>
-            <nav
-                className='flex justify-center items-center h-[200px]'
-                style={{
-                    backgroundImage:
-                        "url(https://i.ibb.co.com/DHdjhyKp/img-3-6d6b3c93.png)",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}>
-                <div className='text-white text-center '>
-                    <h1 className='text-4xl font-bold mb-3'>Our Causes</h1>
-                    <Link to={'/'}><span className='font-semibold text-xl'>Home</span></Link> {'>'}
-                    <span className='font-semibold text-xl'
-                        style={{ color: location.pathname === '/causes-details' ? '#08cc7f' : 'white' }}
-                    >causes</span>
+                <Header></Header>
+                <nav
+                    className='flex justify-center items-center h-[200px]'
+                    style={{
+                        backgroundImage:
+                            "url(https://i.ibb.co.com/DHdjhyKp/img-3-6d6b3c93.png)",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}>
+                    <div className='text-white text-center '>
+                        <h1 className='text-4xl font-bold mb-3'>Our Causes</h1>
+                        <Link to={'/'}><span className='font-semibold text-xl'>Home</span></Link> {'>'}
+                        <span className='font-semibold text-xl'
+                            style={{ color: location.pathname === '/causes-details' ? '#08cc7f' : 'white' }}
+                        >causes</span>
+                    </div>
+                </nav>
+
+                <div className='grid grid-cols-1 md:grid-cols-12 gap-3 w-9/12 mx-auto mt-10'>
+                    <aside className='md:col-span-9'>
+                        <div>
+                            {
+                                details?.map(detail => <CausesDetailsCard key={details.id}
+                                    causes={detail}
+                                ></CausesDetailsCard>)
+                            }
+                        </div>
+                    </aside>
+                    <aside className='md:col-span-3'>right side</aside>
                 </div>
-            </nav>
+            </div>
+
+
 
             <div>
                 {
-                    details?.map(details=><CausesDetailsCard key={details.id}
-                    causes={details}
-                    ></CausesDetailsCard>)
+                    // data.map(singleCauses => (
+                    //     <div>
+                    //         {/* <h1>{singleCauses.title}</h1> */}
+                    //         <div>
+                    //             {
+                    //                 singleCauses.description?.map((desc, index) => {
+                    //                     return (
+                    //                         <>
+                    //                             <div key={index}>
+                    //                                 <h2 className="text-xl font-semibold text-gray-800">
+                    //                                     {desc["1stHeading"]}
+                    //                                 </h2>
+                    //                             </div>
+                    //                             <div>
+                    //                                 <p>
+                    //                                     {desc["details"]}
+                    //                                 </p>
+                    //                                 <h2>{desc.keyPoin?.map((p, i) => <p key={i}>{p}</p>)}</h2>
+
+                    //                             </div>
+                    //                         </>
+                    //                     )
+                    //                 })
+                    //             }
+                    //         </div>
+                    //     </div>
+                    // ))
                 }
             </div>
-        </div>
-
-
-
-        <div>
-            {
-                // data.map(singleCauses => (
-                //     <div>
-                //         {/* <h1>{singleCauses.title}</h1> */}
-                //         <div>
-                //             {
-                //                 singleCauses.description?.map((desc, index) => {
-                //                     return (
-                //                         <>
-                //                             <div key={index}>
-                //                                 <h2 className="text-xl font-semibold text-gray-800">
-                //                                     {desc["1stHeading"]}
-                //                                 </h2>
-                //                             </div>
-                //                             <div>
-                //                                 <p>
-                //                                     {desc["details"]}
-                //                                 </p>
-                //                                 <h2>{desc.keyPoin?.map((p, i) => <p key={i}>{p}</p>)}</h2>
-
-                //                             </div>
-                //                         </>
-                //                     )
-                //                 })
-                //             }
-                //         </div>
-                //     </div>
-                // ))
-            }
-        </div>
 
         </>
     );
--- my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
+++ my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const CausesDetailsCard = ({ causes }) => {
     console.log(causes);
     return (
-        <div>
-            <figure className='w-full h-[400px]'>
-                <img className='w-[600px] h-[600px] object-contain' src={causes.image} alt="" />
-            </figure>
-            <div>
-                <button className='btn bg-[#21c8cb]'>Read More</button>
-                <button className='btn bg-[#21c8cb]'>Donation</button>
-                <button className='btn bg-[#21c8cb]'>Comments</button>
+        <div className='md:flex-row'>
+            <div className='w-full'>
+                <img className='w-full  object-contain' src={causes.image} alt="" />
+            </div>
+            <div className='my-5 space-x-5 p-3 *:border-4 '>
+                <button className='btn bg-[#21c8cb] rounded-3xl'>Read More</button>
+                <button className='btn bg-[#21c8cb] rounded-3xl'>Donation</button>
+                <button className='btn bg-[#21c8cb] rounded-3xl'>Comments</button>
             </div>
         </div>
     );
