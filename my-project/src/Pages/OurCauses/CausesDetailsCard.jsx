621f2a1bafb567c1a8ede6d143309f23f9045930
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-15 00:06:07 +0600

    causes details paragraph option is done

--- my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
+++ my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
@@ -7,10 +7,20 @@ const CausesDetailsCard = ({ causes }) => {
             <div className='w-full'>
                 <img className='w-full  object-contain' src={causes.image} alt="" />
             </div>
-            <div className='my-5 space-x-5 p-3 *:border-4 '>
+            <div className='my-5 space-x-5 md:p-3 *:border-4 '>
                 <button className='btn bg-[#21c8cb] rounded-3xl'>Read More</button>
                 <button className='btn bg-[#21c8cb] rounded-3xl'>Donation</button>
                 <button className='btn bg-[#21c8cb] rounded-3xl'>Comments</button>
+            </div>
+            <div>
+                {causes.description?.map((desc,index)=>(
+                    <div key={index} className=''>
+                        <h1 className='md:text-3xl font-semibold text-center mb-5'>{desc["1stHeading"]}</h1>
+                        <div className='space-y-3 text-justify'>{desc.details?.map((p,i)=>(
+                            <p key={i}>{p}</p>
+                        ))}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
