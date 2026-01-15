3e70c65cb318a1a416d3a1c3d189658b7d995daf
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-15 20:39:54 +0600

    dynamic details is working

--- my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
+++ my-project/src/Pages/OurCauses/CausesDetailsCard.jsx
@@ -13,15 +13,29 @@ const CausesDetailsCard = ({ causes }) => {
                 <button className='btn bg-[#21c8cb] rounded-3xl'>Comments</button>
             </div>
             <div>
-                {causes.description?.map((desc,index)=>(
+                {causes.description?.map((desc, index) => (
                     <div key={index} className=''>
-                        <h1 className='md:text-3xl font-semibold text-center mb-5'>{desc["1stHeading"]}</h1>
-                        <div className='space-y-3 text-justify'>{desc.details?.map((p,i)=>(
+                        <h1 className='md:text-3xl font-semibold mb-5'>{desc["1stHeading"]}</h1>
+                        <div className='space-y-3 text-justify'>{desc.details?.map((p, i) => (
                             <p key={i}>{p}</p>
                         ))}</div>
+
+                        <div>
+                            <h2 className='md:text-3xl font-semibold my-5'>{desc["2ndHeading"]}</h2>
+                            <p className='mb-5'>{desc["2ndDetails"]}</p>
+                            <div className=''>{
+                                desc.keyPoin?.map((list, i) => (
+                                    <div className='flex items-center gap-2'>
+                                        <span className='text-4xl mt-3'>*</span>
+                                        <p key={i}>{list}</p>
+                                    </div>
+                                ))
+                            }</div>
+                        </div>
                     </div>
                 ))}
             </div>
+
         </div>
     );
 };
